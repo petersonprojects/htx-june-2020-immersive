540f9682e8a14ccad4fff9c00e00fe54568c8b6e
Veronica <veronica@digitalcrafts.com>
2020-06-03 16:21:36 -0500

    lecture notes on lists, sequences and for loops

---
+++ week01/3-Wednesday/postLectureNotes/lists.py
@@ -0,0 +1,121 @@
+
+
+# 1. Create a new list called "planets" that holds all of the names of the planets
+# in our solar system. Print the list of planets on the command line.
+
+planet1 = "earth"
+planet2 = "jupiter"
+planet3 = "neptune"
+planet4 = "mars"
+planet5 = "Saturn"
+planet6 = "Mercury"
+planet7 = "Uranus"
+planet8 = "Venus"
+
+# 2. Print out how many elements are in the planet list
+
+
+# 3. Create a while loop that iterates through each of the items in the planet list
+# and changes them to lowercase .
+
+
+planets = ["Earth", "Jupiter", "Neptune", "Mars", "Saturn"]
+	
+# plantes[0] = "earth"
+
+# planets[2] = planets[2].lower()
+
+# index = 0
+# while index < len(planets):
+#     planets[index] = planets[index].lower()
+#     print(planets[index])
+#     index += 1
+    
+
+    
+# print(planets)
+
+
+# 4. Add Pluto to the planet list.
+
+
+# 5. Combine the follwing 2 lists into a list called Houston.
+# Find how many cities are listed in the Houston list
+# Add more cities to the Houston list.
+# Print the list cities (one city per line)
+
+HoustonCities = ["Katy", "Memorial City", "Sugar Land",
+                "The Heights", "River Oaks", "Pasadena"]
+
+ClearLakeCities = ["League City", "Kemah", "Seabrook", "Webster", "El Lago"]
+
+
+Houston = []
+
+index = 0
+
+while index < len(HoustonCities) :
+    
+    Houston.append(HoustonCities[index])
+    index += 1
+    
+
+index = 0
+
+while index <  len(ClearLakeCities):
+    Houston.append(ClearLakeCities[index])
+    index += 1
+    
+print(Houston)
+
+# append, extends, concatenation
+
+
+# 6. Since Pluto isn't really a planet, delete it from the planet list
+
+
+# 7. Create the following lists that are a subset of the Houston list:
+# htx1 = The first 4 cities
+# htx2 = Cities 3-6
+# htx3 = The last 2 cities
+
+# 7. Insert Denver in the Houston list after The Heights
+
+# 8.  Remove the last city from the Houston List
+
+# 9. Get the index of Seabrook from the Houston list
+
+print(Houston.index("Seabrook"))
+
+# 10. Sort the list of cities
+
+# 11. Copy the Houston list to a list called USCities
+
+# 12. Remove all items from the Houston list
+
+# 13. Take the following list and multiply 5 times
+
+nums = [4, 5, 7, 8]
+
+# 14
+
+
+# 15. Reverse the String "DigitalCrafts"
+
+
+# 16.  Crete a range [0, 1, 2, 3, 4, 5]
+
+# 17. Create a range [6, 8, 10, 12, 14, 16]
+
+# 18.  Loop through the planet list using a for loop and print the name of each planet
+
+
+# 19.  Loop through the USCities list and print the name of each city
+
+# 20.  Loop through the following sequence [6, 8, 10, 12, 14, 16] and multiply each number by 5.  Print the result.
+
+# 21. Create a multiplication table for 1-10
+
+# i.e.  1x1 = 1
+#        1x2 = 2
+#        1x3 = 3
