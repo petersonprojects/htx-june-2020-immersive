203d952d76ddb72dca7418bff5770dcec013862f
Veronica <veronica@digitalcrafts.com>
2020-06-08 21:05:14 -0500

    oop lecture notes

--- week02/1-Monday/postLectureNotes/class.py
+++ week02/1-Monday/postLectureNotes/class.py
@@ -1,1 +1,70 @@
 
+
+accounts = {"12345":
+            {"fName": "Chris",
+             "lName": "David",
+             "AccountType": {
+                 "Savings": {
+                     "Balance": 0
+                     "Status": True
+                 }
+                 "Checking": {
+                     "Balance": 100,
+                     "Status": True
+
+                 }
+             }}
+            }
+ 
+
+bank = {
+    "Bank Name": "Digital Crafts",
+    "Address": "123 Sesame Street",
+    "City": "Houston",
+
+}
+
+def deposit(user):
+    pass
+
+def withdrawal(user):
+    pass 
+
+def freeze(user):
+    pass 
+
+def transfer(user):
+    pass 
+
+def CreateNewAccount():
+    pass
+
+def printBankInfo():
+    pass
+
+while True:
+    # print menu
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Freeze")
+    print("4. Transfer")
+    print("5. Create new account")
+    print("6. Exit")
+    print("7. Print banking info")
+    user = int(input("Choose option (1-6): "))
+    if(user  == 1):
+        pass
+    elif(user  == 2):
+        pass
+    elif(user  == 3):
+        pass
+    elif(user  == 4):
+        pass
+    elif(user  == 5):
+        pass
+    elif(user  == 6):
+        pass
+    elif(user  == 7):
+        pass
+    
+    
