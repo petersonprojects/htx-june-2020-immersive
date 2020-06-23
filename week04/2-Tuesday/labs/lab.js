d68239c7b5fed6433af2625350fa1a10ece9e0af
Micah Peterson <micahpeterson@live.com>
2020-06-23 11:19:55 -0500

    fizzbuzz

--- week04/2-Tuesday/labs/lab.js
+++ week04/2-Tuesday/labs/lab.js
@@ -10,20 +10,39 @@ var name = firstName +" " +lastName;
 console.log(name);
 
 // Declare a variable called a with a value of 2 
-
+var a = 2;
 //Declare a variable called b witha value of 3 
-
+var b = 3;
 // Declare a variable x whose value is a + b 
-
+var x = a + b;
 // Print x  to the screen 
-
+console.log(x);
 // Add the string "5" to x and print the result
-
+x = "5" + x;
+console.log(x);
 // Print the number of characters in firstName
-
+console.log(firstName.length);
 
 // If else, fizz buzz
-
+for (i = 1; i <=100; i++)
+{
+    if(i%3==0 && i%5==0)
+    {
+        console.log("fizzbuzz");
+    }
+    else if(i%5==0)
+    {
+        console.log("fizz");
+    }
+    else if(i%3==0)
+    {
+        console.log("buzz");
+    }
+    else
+    {
+        console.log(i);
+    } 
+}
 // ternary operator
 
 
