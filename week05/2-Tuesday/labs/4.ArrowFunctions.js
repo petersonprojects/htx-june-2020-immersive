72ed351dc2a22dec5e410bb17c39cbc9095524a4
Micah Peterson <micahpeterson@live.com>
2020-06-30 10:34:46 -0500

    arrow function notes

--- week05/2-Tuesday/labs/4.ArrowFunctions.js
+++ week05/2-Tuesday/labs/4.ArrowFunctions.js
@@ -4,7 +4,23 @@ function mult(a, b) {
     return a * b;
 }
 
+// as an arrow func
+let mult = (a, b) => (a*b);
+
+// set time out is a broswer api
 setTimeout(function () {
     console.log(mult(5, 10));
 }, 1000)
 
+//written as an arrow function
+setTimeout(()=>{console.log(mult(5,10))}, 1000);
+
+//purpose of arrow functions is for keyword "this"
+// javascript not a typical OOP language
+// did not start as full fledged language
+// when it begun it was meant to make website interactive
+// prototypical language
+// javascript is based on blocks that are linked together
+// containers etc.
+
+// "this" keyword denotes execution context
