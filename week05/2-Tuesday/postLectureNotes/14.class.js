06205d696416e23ee80d5f1779b0a4213d2a9673
Micah Peterson <micahpeterson@live.com>
2020-06-30 14:16:49 -0500

    price of the apple is 2dollars

--- week05/2-Tuesday/postLectureNotes/14.class.js
+++ week05/2-Tuesday/postLectureNotes/14.class.js
@@ -1,24 +1,29 @@
 
 /* 
-Rewrite code blow using ES6 Classes
+Rewrite code below using ES6 Classes
 */
 
+class Fruit{
 
-function Fruit(title, price){
-    this.title = title;
-    this.price = price;
-}
+    constructor(title,price)
+    {
+        this.title = title;
+        this.price = price;
+    }
 
-Fruit.prototype.priceInfo = function() {
-    return `Price of thie ${this.title} is $${this.price}`;
+    priceInfo(){
+        return `Price of the ${this.title} is $${this.price}`;
+    }
 
 }
 
 var apple = new Fruit("Apple", 2);
 console.log(apple.priceInfo());
+
 // Price of the Apple is $2
 
 var orange = new Fruit("Orange", 3);
 console.log(orange.priceInfo());
+
 // Price of the Orange is $3
 
