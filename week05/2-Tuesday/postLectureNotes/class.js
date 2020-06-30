adfe6258c212d069cfbeced98a6934cbbb9da3ef
Veronica <veronica@digitalcrafts.com>
2020-06-30 09:38:56 -0500

    es6

---
+++ week05/2-Tuesday/postLectureNotes/class.js
@@ -0,0 +1,58 @@
+
+
+// var i = 5;
+
+// for (let i = 0; i < 10; i++) {
+
+//     //console.log(i);
+
+// }
+
+
+// var sum = i + 5;
+
+// console.log(sum)
+
+
+// function varTest() {
+
+//     var x = 31;
+
+//     if (true) {
+//       let x = 71;  // same variable!
+//       console.log(x);  // 71
+//     }
+
+//     console.log(x);  // 71
+// }
+
+
+// varTest()
+
+// const PI = 3.14;
+
+// PI = 3;
+
+// console.log(PI)
+
+const arr = [1, 2, 4, 7]
+
+
+arr.push(8)
+
+// arr = [1, 2, 3]
+
+console.log(arr)
+
+
+const obj = {};
+
+obj.newKey = 4; 
+
+console.log(obj);
+
+obj = {key: 56}
+
+
+
+
