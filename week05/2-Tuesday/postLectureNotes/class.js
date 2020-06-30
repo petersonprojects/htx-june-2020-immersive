358c2b6789be95e169bb2c9e3fae583a5c82041a
Micah Peterson <micahpeterson@live.com>
2020-06-30 09:49:49 -0500

    temp literals

--- week05/2-Tuesday/postLectureNotes/class.js
+++ week05/2-Tuesday/postLectureNotes/class.js
@@ -1,12 +1,17 @@
-var i = 5;
+// var i = 5;
 
-for (let i = 0; i<10;i++)
-{
-    console.log(i);
-}
+// for (let i = 0; i<10;i++)
+// {
+//     console.log(i);
+// }
 
-// var is for local space and let it for local space.
+// // var is for local space and let it for local space.
 
-var sum = i + 5;
+// var sum = i + 5;
 
-console.log(sum);
+// console.log(sum);
+
+const name = "Tiger";
+const aged = 13;
+
+console.log(`My cat is named: ${name} and he's ${aged} years old`);
