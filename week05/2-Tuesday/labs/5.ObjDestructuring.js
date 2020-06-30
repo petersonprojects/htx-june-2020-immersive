9b51b9eec9dd32033d786d78297f95765369e8b6
Micah Peterson <micahpeterson@live.com>
2020-06-30 10:25:24 -0500

    commit arrow func

--- week05/2-Tuesday/labs/5.ObjDestructuring.js
+++ week05/2-Tuesday/labs/5.ObjDestructuring.js
@@ -3,20 +3,20 @@
 Declare mult() function that will multiply values of the x,y,z 
 fields of the passed object
 */
-var obj = {
-    x: 5,
-    y: 20,
-    z: 3
-}
+// var obj = {
+//     x: 5,
+//     y: 20,
+//     z: 3
+// }
 
-function mult(obj)
-{
-    let product = obj.x * obj.y * obj.z;
-    return product;
-}
+// function mult(obj)
+// {
+//     let product = obj.x * obj.y * obj.z;
+//     return product;
+// }
 
-var product = mult(obj)
-console.log(product);
+// var product = mult(obj)
+// console.log(product);
 //300
 
 /*
@@ -27,22 +27,22 @@ Sample result:
 If input object doesn't have postsQuantitiy field it should get default value 0.
 */
 
-var person1 = {
-    name: "Mike",
-    info: {
-        country: "Spain",
-        age: 23
-    },
-    postsQuantitiy: 100
-}
+// var person1 = {
+//     name: "Mike",
+//     info: {
+//         country: "Spain",
+//         age: 23
+//     },
+//     postsQuantitiy: 100
+// }
 
-var person2 = {
-    name: "Alice",
-    info: {
-        country: "Italy",
-        age: 25
-    }
-}
+// var person2 = {
+//     name: "Alice",
+//     info: {
+//         country: "Italy",
+//         age: 25
+//     }
+// }
 
 
 
@@ -55,10 +55,24 @@ var person2 = {
 // person(person1);
 
 
-function person(obj)
+// function person(obj)
+// {
+//     var {name: n, info: {}, postsQuantitiy: p} = obj;
+//     console.log(name, info, postsQuantitiy);
+// }
+
+// person(person1)
+
+
+function test1(obj)
 {
-    var {name: n, info: {}, postsQuantitiy: p} = obj;
-    console.log(name, info, postsQuantitiy);
+    //
+    return "hello"
 }
 
-person(person1)
+//if u only have one parameter you can remove the parenthesis
+let test2 = obj => {
+    return "hello"
+}
+
+console.log(test2(1));
