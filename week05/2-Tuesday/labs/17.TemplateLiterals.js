2c7ef06bdc977960fc5c3378d1cc5a2f21b6870e
Micah Peterson <micahpeterson@live.com>
2020-06-30 09:59:44 -0500

    assigning obj to vars

---
+++ week05/2-Tuesday/labs/17.TemplateLiterals.js
@@ -0,0 +1,23 @@
+// let today = new Date();
+
+// var text = `the sum of 4 and 5 is ${today.toLocaleTimeString()}`
+
+// var arr = [0,1,2,3];
+
+// //tedious
+
+// var [a,b,c,d] = arr;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+var luke = {
+    occupation: 'jedi',
+    father: 'anakin'
+}
+
+var occupation = luke.occupation;
+
+console.log(occupation);
