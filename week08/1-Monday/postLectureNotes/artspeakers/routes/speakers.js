8e57ddd4994767212e572c53eab02cd189af40a2
Veronica <veronica@digitalcrafts.com>
2020-07-20 11:11:42 -0500

    artspeakers

---
+++ week08/1-Monday/postLectureNotes/artspeakers/routes/speakers.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+
+const dataFile = require('../data/data.json');
+
+
+router.get('/speakers', (req, res) => {
+
+    //code
+    let pageSpeakers = dataFile.speakers; // [{}, {}, {}]
+    let pagePhotos = [];
+    
+
+    pageSpeakers.forEach((speakerObj)=>{
+        pagePhotos = pagePhotos.concat(speakerObj.artwork);
+    })
+
+    res.render('speakers', {
+        artwork: pagePhotos,
+        speakers: pageSpeakers
+    })
+})
+
+
+router.get('/speakers/:speakerid', (req, res) => {
+
+    let speakerArray = dataFile.speakers;
+
+    let photos = [];
+    let speakers = [];
+
+    speakerArray.forEach((speakerObj)=>{
+        if(speakerObj.shortname == req.params.speakerid){
+
+            photos = photos.concat(speakerObj.artwork)
+            speakers.push(speakerObj)
+        }
+
+    })
+
+    // console.log(`photos: ${photos} speaker object: ${pageSpeakers}`);
+
+    res.render('speakers', {
+        artwork: photos,
+        speakers: speakers
+    })
+
+  
+})
+
+
+
+
+module.exports = router;
