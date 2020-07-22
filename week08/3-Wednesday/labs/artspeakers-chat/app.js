f551e354e79fb7181624bc21370b8ce06dad8240
Micah Peterson <micahpeterson@live.com>
2020-07-22 15:45:30 -0500

    asdfa

--- week08/3-Wednesday/labs/artspeakers-chat/app.js
+++ week08/3-Wednesday/labs/artspeakers-chat/app.js
@@ -1,7 +1,8 @@
 
 const express = require('express');
-
 const app = express();
+
+const socketServer = require('socket.io');
 
 // link to public folder 
 app.use(express.static('public'));
@@ -15,8 +16,27 @@ app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 app.use(require('./routes/feedback'));
 app.use(require('./routes/api'));
+app.use(require('./routes/chat'));
 
-
-app.listen(3000, () => {
+let expressServer = app.listen(3000, () => {
     console.log(`Listening on port 3000`);
 })
+
+// allowing socket server to listen to incoming requests from express
+let io = socketServer(expressServer);
+
+// connect is a keyword in socket.io
+// when any client connects with socket connection then this will be triggered
+io.on('connect', (socket)=>{
+    
+    console.log('User connected.');
+
+    // when client send message to server: from 1 client
+    socket.on('chat room', (msg)=>{
+
+        // server will broadcast msg out to all connected clients
+        io.emit('chat room', msg);
+        
+    });
+
+});
