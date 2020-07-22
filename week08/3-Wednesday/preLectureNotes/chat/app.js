695a2efd8bb5d63bbbe4bd2673f913582c6f62f8
Micah Peterson <micahpeterson@live.com>
2020-07-22 15:29:59 -0500

    chat engine

---
+++ week08/3-Wednesday/preLectureNotes/chat/app.js
@@ -0,0 +1,36 @@
+
+
+const express = require('express');
+const app = express();
+const socket = require('socket.io');
+
+
+//connect to public
+app.use(express.static('public'))
+
+//establish views
+app.set('view engine', 'ejs');
+// second parameter would be changed if views folder was named something else
+app.set('views', 'views');
+
+app.use(require('./routes/chat'))
+
+let server = app.listen(8000,()=>{
+    console.log('listening on port 8000');
+})
+
+let io = socket(server); // passes the server into the socket server
+
+io.on('connect', (socket)=>{
+
+    // this message will be broadcast out to each connected client
+    socket.emit('chatMessage', {msg: "Hello from our backend"})
+
+    socket.on('msgFromClient', (clientMessage)=>{
+
+        // broadcasting to all connected clients
+        io.emit('msgFromServer', clientMessage);
+
+    });
+
+});
---
+++ week08/3-Wednesday/preLectureNotes/chat/routes/chat.js
@@ -0,0 +1,11 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/chat', (req,res)=>{
+
+    res.render('chat');
+})
+
+module.exports = router;
