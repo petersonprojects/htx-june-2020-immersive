eebc3c9ca6f118336852aad58324436aaf257d8d
Micah Peterson <micahpeterson@live.com>
2020-08-19 14:20:18 -0500

    alter an array using hooks

--- week12/3-Wednesday/preLectureNotes/hooks/src/App.js
+++ week12/3-Wednesday/preLectureNotes/hooks/src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react'
+import Hook from './Hook'
 
 class App extends Component {
 
-  
+  constructor(){
+    super()
+
+    this.state ={
+      count: 0,
+      title: "Class Based Components",
+      arr: [1,2,5,6,7]
+    }
+  }
+
   render() {
+
+    const {count, title, arr} = this.state;
     return (
       <>
-        
+        <h1>{title}</h1>
+        {count}
+
+        <br/>
+        {arr.map(a =>  ` ${a * 3}`)}
+        <br/>
+        <button onClick={()=>this.setState({count: count+1})}>Click</button>
+
+        <br/>
+
+        <Hook />
+
       </>
     )
   }
+
 }
 
 export default App
--- week12/3-Wednesday/preLectureNotes/hooks/src/Hook.js
+++ week12/3-Wednesday/preLectureNotes/hooks/src/Hook.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 const Hook = () => {
-  return (
-    <>
-      
-    </>
-  )
+
+    const [count, setCount] = useState(0);
+    const [title, setTitle] = useState("Hook");
+    const [arr, setArr] = useState([1,2,5,6,7]);
+    const [add, setAdd] = useState(0);
+    
+    // const [state, setstate] = useState(initialState)
+
+    // const multiplyArr = (arr) => {
+
+    //     let mult = arr.map(a => {
+    //         return  setArr(a * 3)
+    //     })
+
+    //     return mult;
+    // }
+
+    return (
+        <>
+            <h1>{title}</h1>
+            {count}
+            <br/>
+            {arr.map(a => ` ${a * 3}`)}
+            <br/>
+            <button onClick={()=>setCount(count + 1)}>Click - Hooks</button>
+            <button onClick={()=>setTitle(title + "s")}>Pluralize Title</button>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            Add 10 here: {add}
+            <br/>
+            <button onClick={()=>setAdd(add + 10)}>Add Ten</button>
+        </>
+    )
 }
 
-export default Hook
+export default Hook;
