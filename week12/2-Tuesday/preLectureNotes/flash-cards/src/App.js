0b903e7e89881691a31e73849c413f27b0f0574e
Veronica <veronica@digitalcrafts.com>
2020-08-19 10:36:17 -0500

    lifecycle

---
+++ week12/2-Tuesday/preLectureNotes/flash-cards/src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import QuizBar from './components/QuizBar';
+import FlashCard from './components/FlashCard'
+
+class App extends Component{
+	constructor(){
+		super();
+		this.state = {
+      cardStyle: "Random",
+      ready: false,
+		}
+  }
+
+  userChoice = (cardStyle)=>{
+    this.setState({
+      cardStyle,
+      ready: false
+    })
+  }
+
+  nowReady = ()=>{
+    this.setState({
+      ready: true
+    })
+  }
+
+  render(){
+    console.log(this.state.cardStyle);
+    return (
+      <div className="App justify-content d-flex">
+        <div className="container">
+          
+          <QuizBar userChoice={this.userChoice} />
+          <FlashCard cardStyle={this.state.cardStyle} nowReady={this.nowReady} ready={this.state.ready}  />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ week12/3-Wednesday/preLectureNotes/lifecycle/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Watch from './Watch';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Watch />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
