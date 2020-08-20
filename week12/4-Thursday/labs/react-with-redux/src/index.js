cae30a51651d14b7e0af50a149b6d2d863ef9f50
Micah Peterson <micahpeterson@live.com>
2020-08-20 15:51:52 -0500

    simple react with redux app

--- week12/4-Thursday/labs/react-with-redux/src/index.js
+++ week12/4-Thursday/labs/react-with-redux/src/index.js
@@ -5,7 +5,7 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import counterReducer from './reducers/counterReducer'
 
-let store = createStore(counterReducer) // pass in the reducer
+let store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) // pass in the reducer, devTools
 
 // Provider is what links redux to react
 ReactDOM.render(
