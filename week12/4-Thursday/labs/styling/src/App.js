02c245345506142b7cea3e71489e7f7c6ebda572
Micah Peterson <micahpeterson@live.com>
2020-08-20 10:01:50 -0500

    simple styling in react

--- week12/4-Thursday/labs/styling/src/App.js
+++ week12/4-Thursday/labs/styling/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+
+const styles = {
+  myDivStyles: {
+    backgroundColor: 'pink',
+    fontSize: '50px'
+  },
+  p: {
+    backgroundColor: 'blue',
+    color: 'white'
+  }
+}
 
 const App = () => {
   return (
     <>
-      
+      <div style={styles.myDivStyles}>Hello World</div>
+      <p style={styles.p}>This is an inline style</p>
     </>
   )
 }
