7bcd49f554bc6d941fde01388db96b8ad0ee0ece
Micah Peterson <micahpeterson@live.com>
2020-08-21 16:10:35 -0500

    instruction notes to shopping cart app

--- week12/5-Friday/preLectureNotes/shopping-cart/src/components/Cart.js
+++ week12/5-Friday/preLectureNotes/shopping-cart/src/components/Cart.js
@@ -8,8 +8,9 @@ const Cart = () => {
     const products = useSelector(state => state.productCart);
     const totalCost = useSelector(state => state.totalCost);
 
-    // boostrap
-    // initial state? create products (images, prices, description, title)
+    // use bootstrap
+    // initial state? create products (imageURL, price, description, name)
+    // click on images with titles and see description, price, title and if I recommend it
     // when user clicks on an item it adds it to your cart
     // for every item in your cart you should be able to delete (already there)
     // add a dropdown to increase the number of items you want to put
